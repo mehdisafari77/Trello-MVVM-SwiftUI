80f9a1659f847590ead40327a16c4f03c7aebdc4
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-23 21:20:27 -0700

    Initial Commit

---
+++ Trello-MVVM-SwiftUI/Trello_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trello_MVVM_SwiftUIApp.swift
+//  Trello-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Trello_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
