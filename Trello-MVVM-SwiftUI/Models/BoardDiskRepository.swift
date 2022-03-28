6d63e11340de2dbb98bb207f8d6d3dc71dd16621
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-27 18:45:26 -0700

    board repo bug fix

--- Trello-MVVM-SwiftUI/Models/BoardDiskRepository.swift
+++ Trello-MVVM-SwiftUI/Models/BoardDiskRepository.swift
@@ -6,60 +6,20 @@
 //
 
 import Foundation
-import SwiftUI
 
-struct BoardDropDelegate: DropDelegate {
+struct BoardDiskRepository {
     
-    let board: Board
-    let boardList: BoardList
-    
-    @Binding var lists: [BoardList]
-    @Binding var current: BoardList?
-    
-    private func boardListItemProviders(info: DropInfo) -> [NSItemProvider] {
-        info.itemProviders(for: [BoardList.typeIdentifier])
+    private var savePathURL: URL {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("board.trl")
     }
     
-    private func cardItemProviders(info: DropInfo) -> [NSItemProvider] {
-        info.itemProviders(for: [Card.typeIdentifier])
+    func loadFromDisk() -> Board? {
+        guard let data = try? Data(contentsOf: savePathURL) else { return nil }
+        return try? JSONDecoder().decode(Board.self, from: data)
     }
     
-    func dropEntered(info: DropInfo) {
-        guard
-            !boardListItemProviders(info: info).isEmpty,
-            let current = current,
-            boardList != current,
-            let fromIndex = lists.firstIndex(of: current),
-            let toIndex = lists.firstIndex(of: boardList) else {
-                return
-            }
-        lists.move(fromOffsets: IndexSet(integer: fromIndex), toOffset: toIndex > fromIndex ? toIndex + 1 : toIndex)
-        
-    }
-    
-    func dropUpdated(info: DropInfo) -> DropProposal? {
-        if !cardItemProviders(info: info).isEmpty {
-            return DropProposal(operation: .copy)
-        } else if !boardListItemProviders(info: info).isEmpty {
-            return DropProposal(operation: .move)
-        }
-        return nil
-    }
-    
-    func performDrop(info: DropInfo) -> Bool {
-        let cardItemProviders = cardItemProviders(info: info)
-        for cardItemProvider in cardItemProviders {
-            cardItemProvider.loadObject(ofClass: Card.self) { item, _ in
-                guard let card = item as? Card,
-                      card.boardListId != boardList.id
-                else { return }
-                DispatchQueue.main.async {
-                    board.move(card: card, to: boardList, at: 0)
-                }
-            }
-        }
-        self.current = nil
-        return true
+    func saveToDisk(board: Board) {
+        guard let data = try? JSONEncoder().encode(board) else { return }
+        try? data.write(to: savePathURL, options: .atomic)
     }
 }
-
