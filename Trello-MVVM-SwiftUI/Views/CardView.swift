95bb20895395dd235b586bdc7f257ccc81339e5a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-28 13:49:19 -0700

    create an finalized the code for card view

---
+++ Trello-MVVM-SwiftUI/Views/CardView.swift
@@ -0,0 +1,56 @@
+//
+//  CardView.swift
+//  Trello-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 28/03/2022.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @ObservedObject var boardList: BoardList
+    @StateObject var card: Card
+    
+    var body: some View {
+        HStack {
+            Text(card.content).lineLimit(3)
+            Spacer()
+            Menu {
+                Button("Edit") { handleEditCard() }
+                Button("Delete", role: .destructive) {
+                    boardList.removeCard(card)
+                }
+            } label: {
+                Image(systemName: "ellipsis.rectangle")
+                    .imageScale(.small)
+            }
+        }
+        .padding(8)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color.white)
+        .cornerRadius(4)
+        .shadow(radius: 1, y: 1)
+    }
+    
+    private func handleEditCard() {
+        presentAlertTextField(title: "Edit Card", defaultTextFieldText: card.content) { text in
+            guard let text = text, !text.isEmpty else {
+                return
+            }
+            card.content = text
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    
+    @StateObject static var boardList = Board.stub.lists[0]
+    
+    static var previews: some View {
+        CardView(boardList: boardList, card: boardList.cards[0])
+            .previewLayout(.sizeThatFits)
+            .frame(width: 300)
+    }
+}
+
