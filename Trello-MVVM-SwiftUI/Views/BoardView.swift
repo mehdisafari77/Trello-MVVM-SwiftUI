0168b062ad0a3b247364b28c64da1db6ec9be670
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-28 13:47:55 -0700

    create board view file and fix folder structures

---
+++ Trello-MVVM-SwiftUI/Views/BoardView.swift
@@ -0,0 +1,20 @@
+//
+//  BoardView.swift
+//  Trello-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 28/03/2022.
+//
+
+import SwiftUI
+
+struct BoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardView()
+    }
+}
